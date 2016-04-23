edc5d0ec29bad4bdb19582549ae366cad70bb9d7
Sergio Sánchez <sss4esob@gmail.com>
2016-04-23 20:01:15 +0200

    Recycler View para mostrar listado de Cards View desde Firebase

--- app/src/main/java/com/example/sergio/todo_list_firebase/TodoListActivity.java
+++ app/src/main/java/com/example/sergio/todo_list_firebase/TodoListActivity.java
@@ -1,24 +1,78 @@
 package com.example.sergio.todo_list_firebase;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class TodoListActivity extends AppCompatActivity {
+import com.example.sergio.todo_list_firebase.models.Reminder;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class TodoListActivity extends AppCompatActivity{
+
+    private static String FIREBASE_URL = "https://reminderslist.firebaseio.com/";
+    private static String FIREBASE_CHILD = "reminders";
+
+    @Bind(R.id.toolbar) Toolbar toolbar;
+    @Bind(R.id.fab) FloatingActionButton fab;
+    @Bind(R.id.remindersList) RecyclerView recyclerView;
+    private Firebase firebase;
+    private List<Reminder> reminders;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
+
+        Firebase.setAndroidContext(this);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        Log.d("FIREBASE","Init Firebase");
+        reminders = new ArrayList<Reminder>();
+        firebase = new Firebase(FIREBASE_URL).child(FIREBASE_CHILD);
+        firebase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                Log.d("FIREBASE","There are " + snapshot.getChildrenCount() + " reminders");
+                for (DataSnapshot reminderSnapshot: snapshot.getChildren()) {
+                    Reminder reminder = reminderSnapshot.getValue(Reminder.class);
+                    reminders.add(reminder);
+                }
+                //init recycler view
+                initRecyclerView();
+            }
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                System.out.println("The read failed: " + firebaseError.getMessage());
+            }
+        });
+
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,6 +80,13 @@ public class TodoListActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void initRecyclerView(){
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        RemindersRecyclerViewAdapter adapter = new RemindersRecyclerViewAdapter(reminders,this);
+        recyclerView.setLayoutManager(llm);
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
@@ -48,5 +109,90 @@ public class TodoListActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    static class ReminderViewHolder extends RecyclerView.ViewHolder{
+
+        private ImageView icon;
+        private CardView cv;
+        private TextView name;
+        private TextView desc;
+
+        public ReminderViewHolder(View itemView) {
+            super(itemView);
+            cv = (CardView)itemView.findViewById(R.id.cardview);
+            icon = (ImageView)itemView.findViewById(R.id.icon);
+            name = (TextView)itemView.findViewById(R.id.remindername);
+            desc = (TextView)itemView.findViewById(R.id.reminderdesc);
+        }
+
+        public CardView getCv() {
+            return cv;
+        }
+
+        public void setCv(CardView cv) {
+            this.cv = cv;
+        }
+
+        public TextView getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name.setText(name);
+        }
+
+        public TextView getDesc() {
+            return desc;
+        }
+
+        public void setDesc(String desc) {
+            this.desc.setText(desc);
+        }
+
+        public ImageView getIcon() {
+            return icon;
+        }
+
+        public void setIcon(int icon) {
+            this.icon.setImageResource(icon);
+        }
+    }
+
+    private class RemindersRecyclerViewAdapter extends RecyclerView.Adapter{
+
+        private List<Reminder> reminders;
+        private Context context;
+
+        public RemindersRecyclerViewAdapter(List<Reminder> reminders, Context context) {
+            this.reminders = reminders;
+            this.context = context;
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.reminder_card_view, parent, false);
+            return  new ReminderViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            Reminder reminder = reminders.get(position);
+            ReminderViewHolder reminderHolder = (ReminderViewHolder)holder;
+            reminderHolder.setName(reminder.getName());
+            reminderHolder.setDesc(reminder.getDesc());
+            reminderHolder.setIcon(R.drawable.warning);
+            setAnimation(reminderHolder.getCv());
+        }
+
+        private void setAnimation(View viewToAnimate){
+            Animation animation = AnimationUtils.loadAnimation(context,android.R.anim.slide_in_left);
+            viewToAnimate.startAnimation(animation);
+        }
+
+        @Override
+        public int getItemCount() {
+            return reminders.size();
+        }
     }
 }
